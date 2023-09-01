95c9d7d7e32362b3d018c50c967181e948747b86
Rozumovskyy <rozumovskyy.ir@pg.com>
2023-09-01 14:43:25 +0200

    commit

--- src/ide/index.ts
+++ src/ide/index.ts
@@ -14,9 +14,12 @@ import {
     willSet,
     didSet,
     displayName,
+    selectOptions,
+    baseTypeInfotableProperty,
 } from 'typescriptwebpacksupport/widgetIDESupport';
 
-import widgetIconUrl from '../images/icon.svg';
+import widgetIconUrl from '../images/icon.ide.png';
+import ideIcon from '../images/printer.png';
 
 /**
  * The `@TWWidgetDefinition` decorator marks a class as a Thingworx widget. It can only be applied to classes
@@ -26,137 +29,65 @@ import widgetIconUrl from '../images/icon.svg';
  * Because of this, the `widgetProperties` method is now optional. If overriden, you must invoke the superclass
  * implementation to ensure that decorated aspects are initialized correctly.
  */
-@description('A widget')
-@TWWidgetDefinition('Test', autoResizable)
-class DemoWebpackWidget extends TWComposerWidget {
-    /**
-     * The `@property` decorator can be applied to class members to mark them as widget properties.
-     * This must be applied with the base type of the property as its first parameter.
-     * The decorator can then also receive a series of aspects to apply to that properties as parameters.
-     *
-     * Because of this, the `widgetProperties` method is now optional. If overriden, you must invoke the superclass
-     * implementation to ensure that decorated properties are initialized correctly.
-     */
-    @property('NUMBER', defaultValue(90)) width: number;
+@description('A widget that Prints QR code using Printjs library')
+@TWWidgetDefinition('QrCodePrinterWebpack', autoResizable)
+class QrCodePrinterWebpack extends TWComposerWidget {
 
     /**
-     * When the `@description` decorator is not used, the JSDoc documentation will be used as
-     * the description for the property.
-     */
-    @property('NUMBER', defaultValue(30)) height: number;
-
-    /**
-     * A number of aspects such as `willSet`, `didSet` and `didBind` can be used to specify various callback
-     * that will be invoked when the value of the property is bound or updated by the user through using the composer.
-     */
-    @description('A label to display on the widget.')
-    @property(
-        'STRING',
-        bindingTarget,
-        defaultValue('my value'),
-        willSet('valueWillSet'),
-        didSet('valueDidSet'),
-    )
-    value: string;
-
-    /**
-     * The `@description` decorator can be applied before widget definitions and property, event or service decorators to specify
-     * the description of the decorated class member. That description will appear in the composer.
-     *
-     * A `displayName` aspect may be used to give the property a friendly name with which it will be rendered in the properties
-     * pane in the mashup builder.
-     */
-    @description('Tracks how many times the widget was clicked')
-    @property('NUMBER', bindingSource, nonEditable, displayName('Clicked Amount'))
-    clickedAmount: number;
-
-    /**
-     * Invoked to obtain the URL to this widget's icon.
-     * @return  The URL.
-     */
+    * Invoked to obtain the URL to this widget's icon.
+    * @return  The URL.
+    */
     widgetIconUrl(): string {
         return widgetIconUrl;
     }
+
+    @property('NUMBER', defaultValue(200))
+    width: number;
+
+    @property('NUMBER', defaultValue(120))
+    height: number;
+
+    @description('The size of resultiong QR code')
+    @property('NUMBER', bindingTarget, defaultValue(128))
+    codeSize: number;
+
+    @property('NUMBER', 
+        defaultValue(1), 
+        selectOptions([
+            { text: '7 %', value: '1' },  // 'QRCode.CorrectLevel.L'
+			{ text: '15 %', value: '0' }, // 'QRCode.CorrectLevel.M'
+			{ text: '25 %', value: '3' }, // 'QRCode.CorrectLevel.Q'
+			{ text: '30 %', value: '2' }  // 'QRCode.CorrectLevel.H'
+        ]))
+    Redundancy: number;
+
+    @description('Data to be printed next to QR code. Must contain 2 fields in its datashape: {"name", "value"}')
+    @property('INFOTABLE', bindingTarget, defaultValue({}))
+    DataToPrint: object;
 
     /**
      * Invoked to obtain the HTML structure corresponding to the widget.
      * @return      The HTML structure.
      */
     renderHtml(): string {
-        return `<div class="widget-content widget-demo-viewer">${this.value}</div>`;
+        return (
+            `<div class="widget-content widget-qrcodeprinterwidget" style="display: flex; flex-direction: column; align-items: center; justify-content: center;">` +
+                `<p>Printer Widget</p>` +
+                `<img src="${ideIcon}" alt="Printer Logo" style="max-width: 50px; height: auto;">` +
+                `<p>Invisible at runtime</p>` +
+            `</div>`
+        );
     }
 
-    /**
-     * This method is invoked whenever the `value` property is about to be updated for any reason, either through
-     * direct assignment or because the user has edited its value in the composer,
-     * because it has been specified in the `willSet` aspect of the `value` property.
-     *
-     * Because of this, the `beforeSetProperty` method is now optional. If overriden, you must invoke the superclass
-     * implementation to ensure that decorated properties are updated correctly.
-     *
-     * @param value         Represents the property's new value.
-     * @return              A string, if the new value should be rejected, in which case the returned string will be
-     *                      displayed as an error message to the user. If the value should be accepted, this method should return nothing.
-     */
-    valueWillSet(value: string): string | void {
-        if (value == 'test') return 'Invalid value specified';
-    }
-
-    /**
-     * This method is invoked whenever the `value` property has been updated for any reason, either through
-     * direct assignment or because the user has edited its value in the composer,
-     * because it has been specified in the `didSet` aspect of the `value` property.
-     *
-     * Because of this, the `afterSetProperty` method is now optional. If overriden, you must invoke the superclass
-     * implementation to ensure that decorated properties are handled correctly.
-     *
-     * @param value         Represents the property's new value.
-     * @return              `true` if the widget should be redrawn because of this change, nothing or `false` otherwise.
-     */
-    valueDidSet(value: string): boolean | void {
-        this.jqElement[0].innerText = value;
-    }
-
-    /**
-     * The service decorator defines a service.
-     *
-     * Because of this, the `widgetServices` method is now optional. If overriden, you must invoke the superclass
-     * implementation to ensure that decorated services are initialized correctly.
-     */
-    @description('Prints out a message when invoked')
+    @description('Invoke function to print desired part of the HTML DOM')
     @service
-    testService;
+    print;
 
-    /**
-     * Property declarations can be mixed in with other method and event or service declarations.
-     */
-    @description('A message to display when the widget is clicked.')
-    @property('STRING', bindingTarget, defaultValue('Invoked via decorator'))
-    clickMessage: string;
-
-    /**
-     * The event decorator defines an event.
-     *
-     * Because of this, the `widgetEvents` method is now optional. If overriden, you must invoke the superclass
-     * implementation to ensure that decorated events are initialized correctly.
-     */
-    @description('Triggered when the widget is clicked')
-    @event
-    clicked;
-
-    /**
-     * Invoked after the widget's HTML element has been created.
-     * The `jqElement` property will reference the correct element within this method.
-     */
     afterRender(): void {
-        // add after logic render here
+        // no after-render logic needed
     }
 
-    /**
-     * Invoked when this widget is destroyed. This method should be used to clean up any resources created by the widget
-     * that cannot be reclaimed by the garbage collector automatically (e.g. HTML elements added to the page outside of the widget's HTML element)
-     */
     beforeDestroy(): void {
-        // add dispose logic here
+        // no dispose logic needed at design-time
     }
 }
--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -2,100 +2,49 @@ import {
     TWWidgetDefinition,
     property,
     canBind,
+    didBind,
     TWEvent,
     event,
     service,
+    TWService,
 } from 'typescriptwebpacksupport/widgetRuntimeSupport';
+
+import * as QRCode from 'qrcodejs';
+import * as printJS from 'print-js';
+
+const tableDivBaseID = 'tableDiv-';
+const qrCodeDivBaseID = 'qrcodeDiv-';
 
 /**
  * The `@TWWidgetDefinition` decorator marks a class as a Thingworx widget. It can only be applied to classes
  * that inherit from the `TWRuntimeWidget` class.
  */
 @TWWidgetDefinition
-class DemoWebpackWidget extends TWRuntimeWidget {
-    /**
-     * The `@event` decorator can be applied to class member to mark them as events.
-     * They must have the `TWEvent` type and can be invoked to trigger the associated event.
-     *
-     * Optionally, the decorator can receive the name of the event as its parameter; if it is not specified,
-     * the name of the event will be considered to be the same as the name of the class member.
-     */
-    @event clicked: TWEvent;
-
-    /**
-     * The `@property` decorator can be applied to class member to mark them as events.
-     * The value of the class member and of the associated widget property will be kept in sync.
-     *
-     * The runtime will also automatically update the value of the property for bindings; because of this
-     * the `updateProperty` method becomes optional. If `updateProperty` is overriden, you must invoke
-     * the superclass implementation to ensure that decorated properties are updated correctly.
-     *
-     * Optionally, the decorator can receive a number of aspects as its parameters.
-     */
-    @property clickMessage: string;
-
-    /**
-     * The `canBind` and `didBind` aspects can be used to specify callback methods to execute when the value of
-     * the property is about to be updated or has been updated because of a binding.
-     *
-     * For `canBind`, the method can decide to reject the newly received value.
-     */
-    @property(canBind('valueWillBind')) set value(value: string) {
-        this.internalLogic.createDataElement(this.jqElement[0], value);
-    }
-
-    /**
-     * Optionally, the first parameter of the `@property` decorator can be a string that specifies the
-     * name of the property as it is defined in the IDE class. This can be used to have different names
-     * in the definition and implementation.
-     */
-    @property('clickedAmount') timesClicked: number;
-
-    /**
-     * This method is invoked whenever the `value` property is about to be updated because of a binding,
-     * because it has been specified in the `canBind` aspect of the `value` property.
-     * @param value         Represents the property's new value.
-     * @param info          The complete updatePropertyInfo object.
-     * @return              `true` if the property should update to the new value, `false` otherwise.
-     */
-    valueWillBind(value: string, info: TWUpdatePropertyInfo): boolean {
-        alert(`Value will be updated to ${value}`);
-        return true;
-    }
+class QrCodePrinterWebpack extends TWRuntimeWidget {
 
     /**
      * Invoked to obtain the HTML structure corresponding to the widget.
      * @return      The HTML structure.
      */
     renderHtml(): string {
-        return `<div class="widget-content widget-demo">${this.value}</div>`;
+        return (
+            '<div class="widget-content widget-qrcodeprinterwidget" style="display: none;">' +
+            '<div id="contentToPrint"></div>' +
+            '</div>'
+        );
     }
 
-    internalLogic;
-
-    /**
-     * Invoked after the widget's HTML element has been created.
-     * The `jqElement` property will reference the correct element within this method.
-     */
-    async afterRender(): Promise<void> {
-        this.internalLogic = await import('../common/internalLogic');
-        this.jqElement[0].addEventListener('click', (event: MouseEvent): void => {
-            this.timesClicked++;
-            this.clicked();
-            event.stopPropagation();
+    @service print(): void {
+        this.populateDiv();
+        printJS({
+            printable: "contentToPrint",
+            type: "html",
+            documentTitle: "Print Document"
         });
     }
 
-    /**
-     * The `@service` decorator can be applied to methods to mark them as events.
-     *
-     * The runtime will also automatically call the method when the associated service is invoked.
-     *
-     * Optionally, the decorator can receive the name of the service as its parameter; if it is not specified,
-     * the name of the service will be considered to be the same as the name of the method.
-     */
-    @service testService(): void {
-        alert(this.clickMessage);
+    afterRender(): void {
+        console.log('webpack widger rendered correclty.');
     }
 
     /**
@@ -105,4 +54,101 @@ class DemoWebpackWidget extends TWRuntimeWidget {
     beforeDestroy?(): void {
         // add disposing logic
     }
+
+
+    populateDiv(): void {
+        TW.log.info('About to update divs to print');
+        console.log('About to update divs to print')
+        let contentToPrint = document.getElementById('contentToPrint');
+        this.clearData(contentToPrint);
+        const allData = this.getProperty('DataToPrint')
+        console.log('Retrieved data from property DataToPrint: ');
+        console.log(allData);
+        for (let i = 0; i < allData.length; i++) {
+            // Creating row div that holds all data relevant to one qr code
+            let div = document.createElement('div');
+            div.style.display = "flex"
+            contentToPrint.appendChild(div);
+
+            // Creating table div that holds information relevant to qr code
+            let tableDiv = document.createElement('div');
+            tableDiv.id = tableDivBaseID + i;
+            tableDiv.style.display = "inline-block";
+            console.log('About to create table to with information');
+            this.populateTable(tableDiv, allData[i])
+            div.appendChild(tableDiv);
+            console.log('Table created');
+
+            // Creating spacer div we need it to ensure proper spacing when printing
+            let spacerDiv = document.createElement('div');
+            spacerDiv.style.display = 'inline-block';
+            spacerDiv.style.width = '20px';
+            div.appendChild(spacerDiv);
+
+            // creating qrCode hodler div
+            let qrcodeDiv = document.createElement('div');
+            qrcodeDiv.id = qrCodeDivBaseID + i;
+            qrcodeDiv.style.display = "inline-block";
+            div.appendChild(qrcodeDiv);
+            console.log('About to generate qr code');
+            this.generateQRCode(i, allData[i].code);
+            console.log('qr code created');
+        }
+    };
+
+    clearData(divToClear): void {
+        while (divToClear.firstChild) {
+            divToClear.removeChild(divToClear.firstChild);
+        }
+    };
+
+    populateTable(tableDiv, associatedData): void {
+        let table = document.createElement('table');
+        table.classList.add('myTable');
+        this.createRow(table, 'th', { 'name': 'Property', 'value': 'Value' });
+        for (let i = 0; i < associatedData.info.rows.length; i++) {
+            this.createRow(table, 'td', associatedData.info.rows[i]);
+        }
+        tableDiv.appendChild(table);
+    };
+
+    createRow(table, rowType, row): void {
+        let newRow = document.createElement('tr');
+        newRow.classList.add('myTable');
+
+        let property = document.createElement(rowType);
+        property.classList.add('myTable');
+        property.appendChild(
+            document.createTextNode(row.name));
+
+        let value = document.createElement(rowType);
+        value.classList.add('myTable');
+        value.appendChild(
+            document.createTextNode(row.value));
+
+        newRow.appendChild(property);
+        newRow.appendChild(value);
+        table.appendChild(newRow);
+    };
+
+    generateQRCode(index, code): void {
+        let divId = qrCodeDivBaseID + index;
+        let config = {
+            'width': this.getProperty("CodeSize"),
+            'height': this.getProperty("CodeSize"),
+            'colorDark': "#000000",
+            'colorLight': "#ffffff",
+            'correctLevel': this.getProperty("Redundancy") // Error correction level
+        };
+        try {
+            let generatedCode = new QRCode(divId, config);
+            try {
+                generatedCode.makeCode(code);
+            } catch (e) {
+                console.log('Error generating qr code: ' + e);
+            }
+        } catch (e) {
+            console.log('Error in setup qr code: ' + e);
+        }
+    };
 }
